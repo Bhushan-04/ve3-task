7f3cada6714444ee05953ce342f2ef579dbd8c6d
Bhushan-04 <pilarebhushan4@gmail.com>
2024-12-22 13:19:49 +0530

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 require("dotenv").config();
-const taskRoutes = require('./routes/task');
+const taskRoutes = require('./routes/Task');
 const userRoutes =  require('./routes/User');
 
 
